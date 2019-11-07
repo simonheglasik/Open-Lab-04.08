a585104c254c9e64bd4e8c344d499a3e1c9c92a5
simonheglasik <simonheglasik@gmail.com>
2019-11-07 10:16:21 +0100

    04.08

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-
+using System.Collections.Generic;
 namespace Open_Lab_04._08
 {
     public class StringTools
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            List<string> b = new List<string> { };
+            foreach(string a in strings)
+            {
+                if (a.Length == 4)
+                {
+                    b.Add(a);
+                }
+            }
+            return b.ToArray();
         }
     }
 }
